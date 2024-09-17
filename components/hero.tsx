c09101196560aa6b010de52c4d86df87bb5dc9a8
Zubair <zizdjbd@gmail.com>
2024-09-17 11:07:50 +0600

    homepage

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,25 +19,15 @@ export default function Header() {
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
+        ড্রাইভ প্যাক বিক্রি করার সহজ মাধ্যম{" "}
       </p>
+
+      <p className="text-center">
+        সাইন আপ করার পর আপনি প্যাকেজ তৈরি করতে পারবেন। এবং কাস্টমার আপনার লিংকে
+        ভিজিট করে অর্ডার দিতে পারবে, সেই অর্ডার আপনি দেখতে পারবেন।   <br />     আপনার ইমেইল যদি হয় khalid2@gmail.com তাহলে আপনার শপের লিংক হবে
+        domain.com/khalid2
+      </p>
+
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
